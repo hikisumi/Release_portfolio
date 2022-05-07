97b21114952bde606fd9d60af814593df985d65b
hikisumi <tomatina0920@gmail.com>
2022-05-07 19:49:51 +0900

    トップ変更

--- 01/js/script.js
+++ 01/js/script.js
@@ -7,17 +7,4 @@ $(function(){
     $('#menu-close').click(function(){
         $('#menu-modal').fadeOut();
     });
-    
-    // first表示・非表示
-    $('#first-show').click(function(){
-       var $first =  $('#first-modal');
-        if ($first.hasClass('open')) {
-            $first.removeClass('open');
-            $first.fadeOut();
-        } else {
-            $first.addClass('open');
-            $first.fadeIn();
-        }
-    });
-    
 });
