ad725ff9d3219ce2b7cb8c3daa3192c155c56503
hikisumi <tomatina0920@gmail.com>
2022-05-09 12:14:58 +0900

    js修正

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+/* $(function(){
     // メニューモーダル表示・非表示
     $('#menu-show').click(function(){
        $('#menu-modal').fadeIn();
@@ -7,4 +7,11 @@ $(function(){
     $('#menu-close').click(function(){
         $('#menu-modal').fadeOut();
     });
+}); */
+
+const menu = document.querySelector("#menu-show");
+
+menu.addEventListener("click", () => {
+  console.log("click!!!!");
+  menu.classList.toggle("active");
 });
